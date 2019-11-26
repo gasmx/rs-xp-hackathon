a8ac5a33516bb4ef13ebbb8b9086f54557030351
italo <italoasouzat.i@gmail.com>
2019-11-26 17:18:21 -0300

    login usando redux

--- frontend/rsxp/src/pages/Login/index.js
+++ frontend/rsxp/src/pages/Login/index.js
@@ -1,22 +1,55 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { ButtonToolbar, Button } from 'react-bootstrap';
 
-const Login = () => {
+import { signInRequest } from '../../store/modules/auth/actions';
+import api from '../../services/api';
+
+import {
+    Container,
+    ContainerLogin,
+    Formulario,
+    Info,
+    SubmitLogin,
+} from './styles';
+
+export default function Login({ navigation }) {
+    const dispatch = useDispatch();
+    const [tipoLogin, setTipoLogin] = useState('estudante');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    async function submit(e) {
+        e.preventDefault();
+
+        try {
+            dispatch(signInRequest(email, senha, tipoLogin));
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
-        <ButtonToolbar>
-            <Button variant="primary">Primary</Button>
-            <Button variant="secondary">Secondary</Button>
-            <Button variant="success">Success</Button>
-            <Button variant="warning">Warning</Button>
-            <Button variant="danger">Danger</Button>
-            <Button variant="info">Info</Button>
-            <Button variant="light">Light</Button>
-            <Button variant="dark">Dark</Button>
-            <Button variant="link">Link</Button>
-        </ButtonToolbar>
+        <Container>
+            <ContainerLogin>
+                <Formulario >
+                    <h1>Login</h1>
+                    <label>Acessar como</label>
+                    <select id="tipo" onChange={tipo => setTipoLogin(tipo.target.value)} value={tipoLogin}>
+                        <option value="estudante">Estudante</option>
+                        <option value="empresa">Empresa</option>
+                    </select>
+                    <Info>
+                        <label>E-mail</label>
+                        <input id="email" type="email" onChange={email => setEmail(email.target.value)} value={email} />
+                    </Info>
+                    <Info>
+                        <label>Senha</label>
+                        <input id="senha" type="password" onChange={senha => setSenha(senha.target.value)} value={senha} />
+                    </Info>
+                    <SubmitLogin type="submit" onClick={submit}>Acessar</SubmitLogin>
+                </Formulario>
+            </ContainerLogin>
+        </Container>
     );
 };
-
-export default Login;
