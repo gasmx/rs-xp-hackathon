5e95ad73322055fa481ed920096d2f2beda43d96
italo <italoasouzat.i@gmail.com>
2019-11-26 14:01:40 -0300

    integrando bootstrap

--- frontend/rsxp/src/pages/Login/index.js
+++ frontend/rsxp/src/pages/Login/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Container } from './styles';
 
+import { ButtonToolbar, Button } from 'react-bootstrap';
+
 const Login = () => {
     return (
-        <Container>
-            <h1>Login Page</h1>
-        </Container>
+        <ButtonToolbar>
+            <Button variant="primary">Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="success">Success</Button>
+            <Button variant="warning">Warning</Button>
+            <Button variant="danger">Danger</Button>
+            <Button variant="info">Info</Button>
+            <Button variant="light">Light</Button>
+            <Button variant="dark">Dark</Button>
+            <Button variant="link">Link</Button>
+        </ButtonToolbar>
     );
 };
 
