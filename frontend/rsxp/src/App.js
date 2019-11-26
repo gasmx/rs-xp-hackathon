c33facc211cf5485c63eabdb38f230537efa59fb
italo <italoasouzat.i@gmail.com>
2019-11-26 13:28:29 -0300

    resolvendo conflitos do redux

--- frontend/rsxp/src/App.js
+++ frontend/rsxp/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import history from './services/history';
+
+import { store, persistor } from './store';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes />
-      </Router>
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
--- frontend/rsxp/src/store/index.js
+++ frontend/rsxp/src/store/index.js
@@ -8,7 +8,7 @@ import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
 const sagaMonitor = process.env.NODE_ENV === 'development'
-  ? console.tron.createSagaMonitor()
+  ? console.log()
   : null;
 
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
