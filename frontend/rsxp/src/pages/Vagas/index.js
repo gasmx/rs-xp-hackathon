d3680468f581d293a65427e1b15c2762e06b54ab
Thompson <thompson.msds@gmail.com>
2019-11-26 13:04:16 -0300

    Paginas prontas

---
+++ frontend/rsxp/src/pages/Vagas/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Vagas = () => {
+    return (
+        <Container>
+            <h1>Vagas Page</h1>
+        </Container>
+    );
+};
+
+export default Vagas;
