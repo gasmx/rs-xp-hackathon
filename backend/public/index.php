d39873a1e2f3a9115259f49891ebabb217c92939
Gabriel Angelus <gangeluscf@gmail.com>
2019-11-27 09:09:09 -0300

    Criação da lading page

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,5 +1,20 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    }
+    
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    }
+    
+    exit(0);
+}
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
